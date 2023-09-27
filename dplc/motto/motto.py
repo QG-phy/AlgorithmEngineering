2013ad0f9991875fc5ac81f16eef03867f704508
Season Zou <jijiezou@stu.pku.edu.cn>
2023-09-27 22:14:57 +0800

    zjj first test

--- dplc/motto/motto.py
+++ dplc/motto/motto.py
@@ -5,7 +5,7 @@ from dplc.utils import md5_encoding
 def verifyMotto(P: argparse.Namespace):
     name = P.name
     motto = P.motto
-    is_verified = False
+    is_verified = True
     if name == "DPTechnology":
         is_verified = verifyMottoDPTechnology(motto)
     elif name == "YuzhiZhang":
@@ -18,11 +18,13 @@ def verifyMotto(P: argparse.Namespace):
         is_verified = verifyMottoYiboWang(motto)
     elif name == "YuhangWang":
         is_verified = verifyMottoYuhangWang(motto)
+    elif name == "JijieZou":
+        is_verified = verifyMottoJijieZou(motto)
     else:
         assert RuntimeError("Your input name is not valid for this test!")
 
     if is_verified:
-        print(f"Your input '{motto}' mˇatches with {name}'s motto! ")
+        print(f"Your input '{motto}' matches with {name}'s motto! ")
     else:
         print(f"Your input '{motto}' doesn't match with {name}'s motto! ")
 
@@ -149,3 +151,24 @@ def verifyMottoYuhangWang(public_string: str) -> bool:
     False: otherwise.
     '''
     return md5_encoding(public_string) == "48c9598399fca3a74a1f73a641651a25"
+
+def verifyMottoJijieZou(public_string: str) -> bool:
+    '''
+
+    Verify if a public string is Yuzhi Zhang's motto.
+
+    Parameters:
+    ---------
+    public_string: string, a public string from user input.
+
+    Returns:
+    -------
+    is_verified: bool, if the string matches with Yuzhi Zhang's motto,
+        return True.
+    '''
+    md5_string = md5_encoding(public_string)
+    is_verified = False
+    if md5_string == "fef14ee8266b9bafce64579da7066385":
+        is_verified = True
+    return is_verified
+
